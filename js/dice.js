83c668324f71d60f831174765e826e6910cec8bf
Amber Jones <amberlovescats14@gmail.com>
2019-10-24 08:29:51 -0500

    added dice

--- js/dice.js
+++ js/dice.js
@@ -1,32 +1,52 @@
+let rollResult = document.getElementById('roll-result')
+
+let d4 = document.getElementById('d4')
+const rollD4 = () => {
+    console.log(`D4`)
+    let rolled = Math.floor(Math.random() * 4) + 1;
+    rollResult.innerHTML = rolled
+}
+d4.addEventListener('click', rollD4)
+
+let d6 = document.getElementById('d6')
+const rollD6 = () => {
+    console.log(`D6`)
+    let rolled = Math.floor(Math.random() * 6) + 1;
+    rollResult.innerHTML = rolled
+}
+d6.addEventListener('click', rollD6)
 
 
-const jQuery = require('jquery')
+let d8 = document.getElementById('d8')
+console.log(d8)
+const rollD8 = () => {
+    console.log(`D8`)
+    let rolled = Math.floor(Math.random() * 8) + 1;
+    rollResult.innerHTML = rolled
+}
+d8.addEventListener('click', rollD8)
 
-$('svg').click(function(){
-    let d4 = Math.floor(Math.random() * 4) + 1;
-    let d6 = Math.floor(Math.random() * 6) + 1;
-    let d8 = Math.floor(Math.random() * 8) + 1;
-    let d10 = Math.floor(Math.random() * 10) + 1;
-    let d12 = Math.floor(Math.random() * 12) + 1;
-    let d20 = Math.floor(Math.random() * 20) + 1;
-    switch(this.id){
-        case 'd4':
-            $('.roll-result').html(d4);
-            break;
-        case 'd6':
-            $('.roll-result').html(d6);
-            break;
-        case 'd8':
-            $('.roll-result').html(d8);
-            break;
-        case 'd10':
-            $('.roll-result').html(d10);
-            break;
-        case 'd12':
-            $('.roll-result').html(d12);
-            break;
-        case 'd20':
-            $('.roll-result').html(d20);
-            break;
-    }
-})
+let d10 = document.getElementById('d10')
+const rollD10 = () => {
+    console.log(`D10`)
+    let rolled = Math.floor(Math.random() * 10) + 1;
+    rollResult.innerHTML = rolled
+}
+d10.addEventListener('click', rollD10)
+
+let d12 = document.getElementById('d12')
+const rollD12 = () => {
+    console.log(`D12`)
+    let rolled = Math.floor(Math.random() * 12) + 1;
+    rollResult.innerHTML = rolled
+}
+d12.addEventListener('click', rollD12)
+
+let d20 = document.getElementById('d20')
+const rollD20 = () => {
+    console.log(`D20`)
+    let rolled = Math.floor(Math.random() * 20) + 1;
+    rollResult.innerHTML = rolled
+}
+d20.addEventListener('click', rollD20)
+
